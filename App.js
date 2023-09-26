33e5d9f799c06d6a79a86b38a5620c264890e93c
razeeth11 <razeeth011@gmail.com>
2023-09-26 14:40:11 +0530

    React-native-ToDo

--- App.js
+++ App.js
@@ -1,11 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  Button,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [text, setText] = useState();
+  const [goalList, setGoalList] = useState([]);
+
+  function textInput(inputText) {
+    setText(inputText);
+  }
+
+  function onHandler() {
+    if(text !== ''){
+      setGoalList([...goalList, text]);
+    setText("");
+    }
+  }
+
+  function onDelete(index) {
+    var newArray = goalList.filter((goalItem) => goalItem !== goalList[index]);
+    setGoalList(newArray);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.textInputContainer}>
+        <TextInput
+          value={text}
+          onChangeText={textInput}
+          style={styles.textInput}
+          placeholder="Type Something"
+        />
+        <Button onPress={onHandler} title="Add Goals" />
+      </View>
+      <View style={styles.goalContainer}>
+        <Text style={styles.goalText}>Your Goals</Text>
+        <ScrollView>
+          {goalList.map((item, index) => (
+            <Pressable onPress={() => onDelete(index)} key={index}>
+              <Text style={styles.goalList}>
+                {index + 1} . {item}
+              </Text>
+            </Pressable>
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -13,8 +59,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    marginTop: 100,
+  },
+  textInputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: 20,
+  },
+  textInput: {
+    width: "60%",
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    padding: 5,
+    fontSize: 14,
+    marginRight: 10,
+  },
+  goalContainer: {
+    marginTop: 20,
+    width: "80%",
+  },
+  goalText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  goalList: {
+    marginTop: 5,
+    padding: 5,
+    backgroundColor: "#EB984E",
+    color: "white",
+    borderRadius: 3,
+    fontSize: 16,
   },
 });
